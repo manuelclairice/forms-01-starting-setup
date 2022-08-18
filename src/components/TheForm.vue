e6a03ad5169bbe3e40c40115becffddb97d1593f
Manuel Clairice <m.clairice@rewe-group.at>
2022-08-18 15:03:10 +0200

    Add rating control component

--- src/components/TheForm.vue
+++ src/components/TheForm.vue
@@ -1,8 +1,9 @@
 <template>
   <form @submit.prevent="submitForm">
-    <div class="form-control">
+    <div class="form-control" :class="{invalid: userNameValidity === 'invalid'}">
       <label for="user-name">Your Name</label>
-      <input id="user-name" name="user-name" type="text" v-model="userName" />
+      <input id="user-name" name="user-name" type="text" v-model.trim="userName" @blur="validateInput" />
+      <p v-if="userNameValidity === 'invalid'">Please enter a valid name!</p>
     </div>
     <div class="form-control">
       <label for="age">Your Age (Years)</label>
@@ -19,32 +20,69 @@
     <div class="form-control">
       <h2>What are you interested in?</h2>
       <div>
-        <input id="interest-news" name="interest" type="checkbox" />
+        <input 
+        id="interest-news" 
+        name="interest" 
+        type="checkbox" 
+        value="news" 
+        v-model="interest"/>
         <label for="interest-news">News</label>
       </div>
       <div>
-        <input id="interest-tutorials" name="interest" type="checkbox" />
+        <input 
+        id="interest-tutorials" 
+        name="interest" 
+        type="checkbox" 
+        value="tutorials" 
+        v-model="interest"/>
         <label for="interest-tutorials">Tutorials</label>
       </div>
       <div>
-        <input id="interest-nothing" name="interest" type="checkbox" />
+        <input 
+        id="interest-nothing" 
+        name="interest" 
+        type="checkbox" 
+        value="nothing" 
+        v-model="interest"/>
         <label for="interest-nothing">Nothing</label>
       </div>
     </div>
     <div class="form-control">
       <h2>How do you learn?</h2>
       <div>
-        <input id="how-video" name="how" type="radio" />
+        <input 
+        id="how-video" 
+        name="how" 
+        type="radio"
+        value="video"
+        v-model="how" />
         <label for="how-video">Video Courses</label>
       </div>
       <div>
-        <input id="how-blogs" name="how" type="radio" />
+        <input 
+        id="how-blogs" 
+        name="how" 
+        type="radio"
+        value="blogs" 
+        v-model="how" />
         <label for="how-blogs">Blogs</label>
       </div>
       <div>
-        <input id="how-other" name="how" type="radio" />
+        <input 
+        id="how-other" 
+        name="how" 
+        type="radio"
+        value="other" 
+        v-model="how" />
         <label for="how-other">Other</label>
       </div>
+    </div>
+    <div class="form-control">
+      <rating-control v-model="rating" ></rating-control>
+    </div>
+    <div class="form-control">
+      <input type="checkbox" id="confirm-terms" name="confirm-terms" v-model="confirm" />
+      <label for="confirm-terms">Agree to term of use?</label>
     </div>
     <div>
       <button>Save Data</button>
@@ -53,14 +91,22 @@
 </template>
 
 <script>
+import RatingControl from './RatingControl.vue'
+
 export default {
+  components: {
+    RatingControl
+  },
   data() {
     return {
         userName: '',
         userAge: null,
         referrer: 'wom',
-        // interest: '',
-        // how: ''
+        interest: null,
+        how: null,
+        confirm: false,
+        rating: null,
+        userNameValidity: 'pending'
       
     };
   },
@@ -75,7 +121,27 @@ export default {
     this.userAge = null;
     console.log('Referrer: ' + this.referrer);
     this.referrer = 'wom';
+    console.log('Checkboxes');
+    console.log(this.interest);
+    console.log('Radio buttons');
+    console.log(this.how);
+    this.interest = null;
+    this.how = null;
+    console.log('Confirm?');
+    console.log(this.confirm);
+    this.confirm = false;
+    console.log('Rating');
+    console.log(this.rating);
+    this.rating = null;
+    
   },
+  validateInput() {
+    if (this.userName === '') {
+      this.userNameValidity = 'invalid';
+    } else {
+      this.userNameValidity = 'valid';
+    }
+  }
  },
 };
 </script>
@@ -93,6 +159,14 @@ form {
 .form-control {
   margin: 0.5rem 0;
 }
+
+.form-control.invalid input {
+  border-color: red;
+}
+.form-control.invalid label {
+  color: red;
+}
+
 
 label {
   font-weight: bold;
